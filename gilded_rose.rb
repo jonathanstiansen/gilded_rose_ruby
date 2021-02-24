bc9a660e8d515e633346b16f0d0fd6c35c313180
Jonathan Stiansen <jonathan.stiansen@gmail.com>
2021-02-24 16:52:51 -0700

    removed conditional

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,7 +4,7 @@ end
 
 def update_quality(items)
   items.each do |item|
-    ProductFactory.new(item).age
+    ProductFactory.new.create(item).age
   end
 end
 
@@ -22,17 +22,19 @@ Item = Struct.new(:name, :sell_in, :quality)
 #   Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20),
 #   Item.new("Conjured Mana Cake", 3, 6),
 # ]
-
-class ProductFactory
+class Product
   def initialize(item)
     @item = item
   end
+
+  def age_quality
+    @item.quality -= 1
+  end
+
   def age
     if @item.name != 'Aged Brie' && @item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if @item.quality.positive?
-        if normal_item?(@item)
-          @item.quality -= 1
-        end
+          age_quality
       end
     else
       if @item.quality < 50
@@ -72,5 +74,31 @@ class ProductFactory
         end
       end
     end
+  end
+end
+
+class NormalProduct < Product
+  def age_quality
+    @item.quality -= 1
+  end
+end
+
+class ProductFactory
+  def create(item)
+    if normal_item?(item)
+      NormalProduct.new item
+    else
+      LegendaryProduct.new item
+    end
+  end
+
+  private
+  def normal_item?(item)
+    item.name != 'Sulfuras, Hand of Ragnaros'
+  end
+end
+
+class LegendaryProduct < Product
+  def age_quality
   end
 end
